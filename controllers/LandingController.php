82250b2525d55964d4c92d0a6692e7660451433f
NajmiNasrudin <najmilatif93@gmail.com>
2026-05-31 13:53:10 +0800

    LandingController: allow ?preview=1 to bypass dashboard redirect

--- controllers/LandingController.php
+++ controllers/LandingController.php
@@ -8,8 +8,8 @@ class LandingController extends \App\Core\Controller
 {
     public function index(): void
     {
-        // Logged-in users go straight to dashboard
-        if (Auth::check()) {
+        // Logged-in users go straight to dashboard (unless ?preview=1)
+        if (Auth::check() && ($_GET['preview'] ?? '') !== '1') {
             $this->redirect('/dashboard');
         }
 
